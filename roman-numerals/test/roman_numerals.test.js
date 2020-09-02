891f42ea82784f46ea885bb06dd129262a3e9acd
MariaesSegura <msegber@gmail.com>
2020-09-02 17:07:22 +0200

    refactor

--- roman-numerals/test/roman_numerals.test.js
+++ roman-numerals/test/roman_numerals.test.js
@@ -1,41 +1,37 @@
 let { RomanNumerals } = require('../src/roman_numerals');
 
 describe('RomanNumerals', function () {
-  it('convert number 1 to Roman Numeral', function () {
-    let romanNumerals = new RomanNumerals();
+  let romanNumerals;
 
+  beforeEach(() => {
+    romanNumerals = new RomanNumerals();
+  });
+
+  it('convert number 1 to Roman Numeral', function () {
     let number = romanNumerals.convert(1);
 
     expect(number).toBe('I');
   });
 
   it('convert number 5 to Roman Numeral', function () {
-    let romanNumerals = new RomanNumerals();
-
     let number = romanNumerals.convert(5);
 
     expect(number).toBe('V');
   });
 
   it('convert number 10 to Roman Numeral', function () {
-    let romanNumerals = new RomanNumerals();
-
     let number = romanNumerals.convert(10);
 
     expect(number).toBe('X');
   });
 
   it('convert number 50 to Roman Numeral', function () {
-    let romanNumerals = new RomanNumerals();
-
     let number = romanNumerals.convert(50);
 
     expect(number).toBe('L');
   });
 
   it('convert number 100 to Roman Numeral', function () {
-    let romanNumerals = new RomanNumerals();
-
     let number = romanNumerals.convert(100);
 
     expect(number).toBe('C');
